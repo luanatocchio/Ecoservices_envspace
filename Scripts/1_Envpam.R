1cc98459f9a801310daa2e6e7fe067470c32b531
BrunoVilela <brunovilelasilva@hotmail.com>
2026-03-19 17:08:53 -0300

    Relation between ecosytem service and env space metrics

--- Scripts/1_Envpam.R
+++ Scripts/1_Envpam.R
@@ -8,6 +8,7 @@ library(letsR)
 library(terra)
 library(geodata)
 library(tidyverse)
+library(gam)
 
 # Load Data ---------------------------------------------------------------
 ## Env data
@@ -35,7 +36,7 @@ envs <- lets.addvar(pam_mammals, clim_br, onlyvar = TRUE)
 colnames(envs) <- c("Temperature", "Preciptation (Log)")
 
 # Create Env pam
-res <- lets.envpam(pam_mammals, envs,   n_bins = 20)
+res <- lets.envpam(pam_mammals, envs,   n_bins = 30)
 lets.plot.envpam(res,
                  world = TRUE)
 
@@ -43,27 +44,52 @@ lets.plot.envpam(res,
 
 # Map ecosystem services -------------------------------------------------
 # Map Service 
-res_map <- lets.maplizer.env(res, 
-                             y = ES$total, 
-                             z = ES$species,
-                             func = sum) # Change here for 
+es_names <- names(ES)[-1]
+data_plot <- list()
+gam_results <- list()
+# Mantain cells without zero
+keep <- res$Presence_and_Absence_Matrix_env[, 1]
+
+for (i in seq_along(es_names)) {
+  res_map <- lets.maplizer.env(
+    res,
+    y = ES[[es_names[i]]],
+    # Change for each ecosystem service
+    z = ES$species,
+    func = sum
+  ) # Change here for mean, sum, variability
+  out <- lets.envcells(res)
+  # lets.plot.envpam(res_map) # for ploting (if you want to save it)
+  
+  abund_env   <- res_map$Matrix_env[, -(1:3), drop = FALSE]
+  data_plot[[i]] <- 
+    tibble(es = es_names[i], Abundance = abund_env[keep, 1], out[keep, -1]) %>%
+    drop_na() %>% 
+    # mutate(Frequency_log = log(Frequency)) %>%
+    select(c(1:3, 7, 15)) %>% # remove variables 
+    pivot_longer(3:5)
+  
+  # STATISTICAL MODEL
+  # gam_results[[i]] <- gam(Abundance ~ ) Dplyr roda modelo para as 3 variaveis e guardar numa (salvar numa lista)
+}
+
+# Combine all
+out <- do.call(bind_rows, data_plot)
 
 # Relate to frequency
-out <- lets.envcells(res)
 
-abund_env   <- res_map$Matrix_env[, -(1:3), drop = FALSE]
 
-# Mantain cells without zero
-keep <- res$Presence_and_Absence_Matrix_env[, 1] & abund_env > 0
-abund_env <- abund_env[keep]
-
-tibble(abund_env, out[keep, -1]) %>%
-  mutate(Frequency_log = log(Frequency)) %>%
-  # select(-) %>% 
-  pivot_longer(-abund_env) %>% 
-  ggplot(aes(y = abund_env, x = value)) +
+# Plot
+out %>%
+  ggplot(aes(y = Abundance, x = value)) +
   geom_smooth() +
   #stat_smooth(method = "lm", formula = y ~ x + I(x^2), size = 1) +
   geom_point() +
-  facet_wrap(.~name, scales = "free")
+  theme_bw() +
+  facet_grid(es~name, scales = "free")
 
+
+ggsave("Figures/es_env_relationships.tif",
+       height = 60,
+       width = 20, 
+       units = "cm")
